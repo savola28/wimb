247a7c74127b8cdd91e34c201cf3b2cdcd271bd3
savola28 <seppo.k.savolainen@gmail.com>
2014-09-07 17:35:38 +0000

    Map is reserved word in javascript

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 /** @jsx React.DOM */
 var Menu = require('./Menu.jsx'),
 	MainView = require('./MainView.jsx'),
-	Map = require('./Map.jsx');
+	MapComponent = require('./Map.jsx');
 
 module.exports = React.createClass({
 	render: function() {
@@ -9,7 +9,7 @@ module.exports = React.createClass({
 			<div className="app">
 			    <Menu views={['home', 'map']} activeView={this.props.activeView} />
 			    <MainView activeView={this.props.activeView} />
-			    <Map activeView={this.props.activeView} />
+			    <MapComponent activeView={this.props.activeView} />
 		    </div>
 		);
 	}
