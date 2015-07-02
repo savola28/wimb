d3a94041519ad155198d2d5d111a0ba6b1c3e7d4
savola28 <seppo.k.savolainen@gmail.com>
2015-07-02 08:45:54 +0000

    code formatting

--- src/favoriteStorage.js
+++ src/favoriteStorage.js
@@ -1,38 +1,41 @@
 module.exports = {
-    add: function(storageKey, id, item){
+    add: function(storageKey, id, item) {
         var storage = this._getStorage(storageKey),
             index = storage.items.push(item) - 1;
         storage.ids[id] = index;
         this._setStorage(storageKey, storage);
     },
-    
-    remove: function(storageKey, id){
+
+    remove: function(storageKey, id) {
         var storage = this._getStorage(storageKey);
         storage.items.splice(storage.ids[id]);
         delete storage.ids[id];
         this._setStorage(storageKey, storage);
     },
-    
-    exists: function (storageKey, id){
+
+    exists: function(storageKey, id) {
         var storage = this._getStorage(storageKey);
-        return (typeof storage.ids[id] !== "undefined");        
+        return (typeof storage.ids[id] !== 'undefined');
     },
-    
-    get: function(storageKey, id){
+
+    get: function(storageKey, id) {
         var storage = this._getStorage(storageKey);
         return storage.items[storage.ids[id]];
     },
-    
-    getAll: function (storageKey){
+
+    getAll: function(storageKey) {
         var storage = this._getStorage(storageKey);
         return storage.items;
     },
-    
-    _getStorage: function(key){
-        return JSON.parse(localStorage.getItem(key)) || {items: [], ids: {}};
+
+    _getStorage: function(key) {
+        return JSON.parse(localStorage.getItem(key)) || {
+            items: [],
+            ids: {}
+        };
     },
-    
-    _setStorage: function(key, value){
+
+    _setStorage: function(key, value) {
         return localStorage.setItem(key, JSON.stringify(value));
     }
 };
